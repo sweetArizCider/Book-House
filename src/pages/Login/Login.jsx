8680180e3221956fc716974a69ac0fe445a5adf8
applecider <arizah.applecider+github@gmail.com>
2025-03-30 01:45:21 -0600

    Add module.css

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './Login.css'; 
+import styles from './Login.module.css';
 import Fondo from '../../assets/img/fondo.png';
 import Logo from '../../assets/img/logo.png';
 
@@ -20,50 +20,44 @@ function Login() {
      };
    
      return (
-       <div className="login-container">
-        <div className='contenido'>
-          <div className='logo-container'>
-              <img src={Logo} alt="Logo de la aplicación" />
-            </div>
-         
-         <form onSubmit={handleSubmit} className="login">
-           <h1>Iniciar Sesión</h1>
-           <h3>¿No tienes una cuenta? <span>registrarse ahora</span></h3>
-   
-           <div className="input-field">
-             <label htmlFor="usuario">Nombre de Usuario</label>
-             <input
-               id="usuario"
-               name="usuario"
-               type="text"
-               value={formData.usuario}
-               onChange={handleChange}
-               placeholder="Nombre de Usuario"
-             />
+       <div className={styles['login-container']}>
+         <div className={styles.contenido}>
+           <div className={styles['logo-container']}>
+             <img src={Logo} alt="Logo de la aplicación" />
            </div>
-   
-           <div className="input-field">
-             <label htmlFor="contraseña">Contraseña</label>
-             <input
-               id="contraseña"
-               name="contraseña"
-               type="password"
-               value={formData.contraseña}
-               onChange={handleChange}
-               placeholder="Contraseña"
-             />
-           </div>
-   
-           <button className='buttonContinuar' type="submit">Continuar</button>
-           <p>Al hacer clic en 'Continuar', usted acepta que sus datos personales sean utilizados de acuerdo con nuestra política de privacidad.</p>
-         </form>
-          </div>
-         <div  className='fondo'>
-          <img src={Fondo} alt="Fondo de pantalla" />
+           <form onSubmit={handleSubmit} className={styles.login}>
+             <h1>Iniciar Sesión</h1>
+             <h3>¿No tienes una cuenta? <span>registrarse ahora</span></h3>
+             <div className={styles['input-field']}>
+               <label htmlFor="usuario">Nombre de Usuario</label>
+               <input
+                 id="usuario"
+                 name="usuario"
+                 type="text"
+                 value={formData.usuario}
+                 onChange={handleChange}
+                 placeholder="Nombre de Usuario"
+               />
+             </div>
+             <div className={styles['input-field']}>
+               <label htmlFor="contraseña">Contraseña</label>
+               <input
+                 id="contraseña"
+                 name="contraseña"
+                 type="password"
+                 value={formData.contraseña}
+                 onChange={handleChange}
+                 placeholder="Contraseña"
+               />
+             </div>
+             <button className="buttonContinuar" type="submit">Continuar</button>
+             <p>Al hacer clic en 'Continuar', usted acepta que sus datos personales sean utilizados de acuerdo con nuestra política de privacidad.</p>
+           </form>
          </div>
-          
-        </div>
-         
+         <div className={styles.fondo}>
+           <img src={Fondo} alt="Fondo de pantalla" />
+         </div>
+       </div>
      );
   }
   
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './Register.css'; 
+import styles from './Register.module.css'; 
 import Fondo from '../../assets/img/fondo.png';
 import Logo from '../../assets/img/logo.png';
 
@@ -24,98 +24,95 @@ function Register() {
   };
 
   return (
-    <div className="register-container">
-      <div className='fondo'>
+    <div className={styles['register-container']}>
+      <div className={styles.fondo}>
         <img src={Fondo} alt="Fondo de pantalla" />
       </div>
-      <div className='contenido'>
-      <div className='logo-container'>
-        <img src={Logo} alt="Logo de la aplicación" />
-      </div>
+      <div className={styles.contenido}>
+        <div className={styles['logo-container']}>
+          <img src={Logo} alt="Logo de la aplicación" />
+        </div>
       
-      <form onSubmit={handleSubmit} className="registro">
-        <h1>¡Empieza ahora!</h1>
-        <h3>¿Ya tienes una cuenta? <span>Iniciar sesión</span></h3>
+        <form onSubmit={handleSubmit} className={styles.registro}>
+          <h1>¡Empieza ahora!</h1>
+          <h3>¿Ya tienes una cuenta? <span>Iniciar sesión</span></h3>
 
-        <div className="input-field">
-          <label htmlFor="nombre">Nombre</label>
-          <input
-            id="nombre"
-            name="nombre"
-            type="text"
-            value={formData.nombre}
-            onChange={handleChange}
-            placeholder="Nombre"
-          />
-        </div>
+          <div className={styles['input-field']}>
+            <label htmlFor="nombre">Nombre</label>
+            <input
+              id="nombre"
+              name="nombre"
+              type="text"
+              value={formData.nombre}
+              onChange={handleChange}
+              placeholder="Nombre"
+            />
+          </div>
 
-        <div className="input-field">
-          <label htmlFor="apellidoPaterno">Apellido Paterno</label>
-          <input
-            id="apellidoPaterno"
-            name="apellidoPaterno"
-            type="text"
-            value={formData.apellidoPaterno}
-            onChange={handleChange}
-            placeholder="Apellido Paterno"
-          />
-        </div>
+          <div className={styles['input-field']}>
+            <label htmlFor="apellidoPaterno">Apellido Paterno</label>
+            <input
+              id="apellidoPaterno"
+              name="apellidoPaterno"
+              type="text"
+              value={formData.apellidoPaterno}
+              onChange={handleChange}
+              placeholder="Apellido Paterno"
+            />
+          </div>
 
-        <div className="input-field">
-          <label htmlFor="apellidoMaterno">Apellido Materno</label>
-          <input
-            id="apellidoMaterno"
-            name="apellidoMaterno"
-            type="text"
-            value={formData.apellidoMaterno}
-            onChange={handleChange}
-            placeholder="Apellido Materno"
-          />
-        </div>
+          <div className={styles['input-field']}>
+            <label htmlFor="apellidoMaterno">Apellido Materno</label>
+            <input
+              id="apellidoMaterno"
+              name="apellidoMaterno"
+              type="text"
+              value={formData.apellidoMaterno}
+              onChange={handleChange}
+              placeholder="Apellido Materno"
+            />
+          </div>
 
-        <div className="input-field">
-          <label htmlFor="email">Correo Electrónico</label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            value={formData.email}
-            onChange={handleChange}
-            placeholder="Correo Electrónico"
-          />
-        </div>
+          <div className={styles['input-field']}>
+            <label htmlFor="email">Correo Electrónico</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Correo Electrónico"
+            />
+          </div>
 
-        <div className="input-field">
-          <label htmlFor="usuario">Nombre de Usuario</label>
-          <input
-            id="usuario"
-            name="usuario"
-            type="text"
-            value={formData.usuario}
-            onChange={handleChange}
-            placeholder="Nombre de Usuario"
-          />
-        </div>
+          <div className={styles['input-field']}>
+            <label htmlFor="usuario">Nombre de Usuario</label>
+            <input
+              id="usuario"
+              name="usuario"
+              type="text"
+              value={formData.usuario}
+              onChange={handleChange}
+              placeholder="Nombre de Usuario"
+            />
+          </div>
 
-        <div className="input-field">
-          <label htmlFor="contraseña">Contraseña</label>
-          <input
-            id="contraseña"
-            name="contraseña"
-            type="password"
-            value={formData.contraseña}
-            onChange={handleChange}
-            placeholder="Contraseña"
-          />
-        </div>
+          <div className={styles['input-field']}>
+            <label htmlFor="contraseña">Contraseña</label>
+            <input
+              id="contraseña"
+              name="contraseña"
+              type="password"
+              value={formData.contraseña}
+              onChange={handleChange}
+              placeholder="Contraseña"
+            />
+          </div>
 
-        <button className='buttonContinuar' type="submit">Continuar</button>
-        <p>Al hacer clic en 'Continuar', usted acepta que sus datos personales sean utilizados de acuerdo con nuestra política de privacidad.</p>
-      </form>
+          <button className="buttonContinuar" type="submit">Registrarse</button>
+        </form>
+      </div>
     </div>
-
-      </div>
-      
   );
 }
 
